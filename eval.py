749eb71a58bc279d10c1af396d7cd031f793ee6c
Ed-war-d <sloter666@gmail.com>
2016-08-03 11:04:49 +0300

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,55 @@
+
+import sys
+import mxnet as mx
+import logging
+import numpy as np
+from skimage import io, transform
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+prefix = "model/Inception"
+num_round = 9
+model = mx.model.FeedForward.load(prefix, num_round, ctx=mx.gpu(), numpy_batch_size=1)
+
+mean_img = 117
+
+synset = [l.strip() for l in open('model/synset.txt').readlines()]
+
+def PreprocessImage(path, show_img=False):
+    # load image
+    img = io.imread(path)
+    print("Original Image Shape: ", img.shape)
+    # we crop image from center
+    short_egde = min(img.shape[:2])
+    yy = int((img.shape[0] - short_egde) / 2)
+    xx = int((img.shape[1] - short_egde) / 2)
+    crop_img = img[yy : yy + short_egde, xx : xx + short_egde]
+    # resize to 224, 224
+    resized_img = transform.resize(crop_img, (224, 224))
+    if show_img:
+        io.imshow(resized_img)
+    # convert to numpy.ndarray
+    sample = np.asarray(resized_img) * 256
+    # swap axes to make image from (224, 224, 4) to (3, 224, 224)
+    sample = np.swapaxes(sample, 0, 2)
+    sample = np.swapaxes(sample, 1, 2)
+    # sub mean 
+    normed_img = sample - mean_img
+    normed_img = normed_img.reshape(1, 3, 224, 224)
+    return normed_img
+
+
+# Get preprocessed batch (single image batch)
+batch = PreprocessImage(sys.argv[1], True)
+# Get prediction probability of 1000 classes from model
+prob = model.predict(batch)[0]
+# Argsort, get prediction index from largest prob to lowest
+pred = np.argsort(prob)[::-1]
+# Get top1 label
+top1 = synset[pred[0]]
+print("Top1: ", top1)
+# Get top5 label
+top5 = [synset[pred[i]] for i in range(5)]
+print("Top5: ", top5)
+
